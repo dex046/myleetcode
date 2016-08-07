af7a873aa43c37e3cad626618c3da0f7a04a1d6b
dwx <527264236@qq.com>
2016-08-07 11:27:09 +0800

    8.7.11.28

---
+++ Binary Tree Level Order Traversal_102.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<TreeNode*> myque;
+        vector<vector<int>> res;
+        if(!root)
+        {
+            return res;
+        }
+        myque.push(root);
+        
+        while(!myque.empty())
+        {
+            int size = myque.size();
+            vector<int> vec;
+            for(int i = 0; i < size; ++i)
+            {
+                TreeNode *temp = myque.front();
+                myque.pop();
+                vec.push_back(temp->val);
+                if(temp->left)
+                {
+                    myque.push(temp->left);
+                }
+                if(temp->right)
+                {
+                    myque.push(temp->right);
+                }
+            }
+            res.push_back(vec);
+        }
+        
+        return res;
+    }
+};
