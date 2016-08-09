3529b38ad172da28f0c2f24da9cad03ff8fffeb4
dwx <527264236@qq.com>
2016-08-09 20:28:50 +0800

    8.9.20.28

--- Merge Sorted Array_88.cpp
+++ Merge Sorted Array_88.cpp
@@ -1,5 +1,17 @@
 class Solution {
 public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+    {
+        int r1 = m-1, r2 = n-1, r = m+n-1;
+        while(r1>=0 && r2>=0)
+            nums1[r--] = nums1[r1]>nums2[r2]? nums1[r1--] : nums2[r2--];
+        while(r2 >= 0)
+            nums1[r--] = nums2[r2--];
+    }
+};
+
+class Solution {
+public:
 void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 	if (n == 0)
 		return;
